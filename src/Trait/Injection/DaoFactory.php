c950d8e4ae079658753438e1eb8f69562394001e
Sébastien Kus <k.sebastien@la-becanerie.com>
2021-10-20 12:01:55 +0200

    Add trait to inject dao factory

---
+++ src/Trait/Injection/DaoFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file is a part of sebk/small-swoft-auth
+ * Copyright 2021 - Sébastien Kus
+ * Under GNU GPL V3 licence
+ */
+
+namespace Sebk\SmallOrmSwoft\Trait\Injection;
+
+use Sebk\SmallOrmSwoft\Factory\Dao;
+use Swoft\Bean\Annotation\Mapping\Inject;
+
+trait DaoFactory
+{
+    /**
+     * @Inject()
+     *
+     * @var Dao
+     */
+    private $daoFactory;
+}
